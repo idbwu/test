e4741433e1a7e052ffd235af076f9ec41832a6aa
idbwu <125094957+idbwu@users.noreply.github.com>
2025-02-01 08:56:23 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+// Добавим плавную прокрутку к разделам
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
