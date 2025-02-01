743335ad5f362c0f5c5665020b6f40f78c62ae51
idbwu <125094957+idbwu@users.noreply.github.com>
2025-02-01 11:50:09 +0300

    Create Header.js

---
+++ Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header className="App-header">
+      <h1>Привет, я [Ваше имя]</h1>
+      <p>Мой профессиональный опыт и проекты.</p>
+    </header>
+  );
+}
+
+export default Header;
