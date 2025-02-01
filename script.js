b397b53cacf64b75ea0b29969fb2d18501e8ad9a
idbwu <125094957+idbwu@users.noreply.github.com>
2025-02-01 11:40:18 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 const themeToggleButton = document.getElementById('theme-toggle');
 const body = document.body;
 
-// Сменить тему
 themeToggleButton.addEventListener('click', () => {
     body.classList.toggle('dark-theme');
 });
